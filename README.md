1d4f9d49e96eb66d789009a436f86bc8e4da28ab
jableable <jable@iu.edu>
2024-04-09 12:43:33 -0400

    Create g_r_c_india, project-summary, README
    g_r_c_india is a small alteration of g_r_c which
    has decreased areas of searching to deal with
    denser road system of India.
    
    project-summary could use some editing. In
    particular, can we make our KPIs more
    specific/fleshed out in terms of dollar amounts
    and money saved for the government?
    
    I was also thinking about including GPS navigation
    companies as stakeholders, but for space reasons I
    left that out.
    
    README added with nice pictures.

---
+++ README.md
@@ -0,0 +1,82 @@
+Here's a list of steps I think we should follow to get started. Of course, this list is not comprehensive, but it should give us a good starting point. **Think about where you fit in!**
+
+When you're working on a piece of the project, try to follow [these](https://github.com/jableable/road-project/blob/main/collaboration-guide.md) branching and pushing conventions.
+
+We'll likely switch to using "Issues" to request/record feature additions and bug fixes.
+
+# Dataset
+The plan is to train our model on 10,000+ images (likely .png) of satellite data of various parts of cities involving some number of roads/highways, such as this:
+
+<p align = "center">
+<img src="./assets/images/la_sat_map.png" width="400" />
+</p>
+
+We'll initially limit ourselves to images with at most 10 crossings (upgrading to more crossings if time allows). Here are things that need doing/exploring:
+
+1. Use Google Maps API to download satellite images of a fixed size based on GPS coordinates
+    * Not sure what fixed size/resolution/zoom should be
+2. Get map bounding box of "crossing_counter_cleaned.py" to agree with bounding box of OpenStreetMap API
+    * These should also agree with Google Maps API images
+
+&nbsp;
+
+# Labeling the Dataset
+
+I've already coded "crossing_counter_cleaned.py" to help us automate crossing counts based on the underlying graph representation from OpenStreetMaps. You can see the crossings from the satellite map above as black vertices here:
+
+<p align = "center">
+<img src="./assets/images/la_combined_map.png" width="400" />
+</p>
+
+Here are things that need doing/exploring:
+
+1. Rewrite "crossing_counter_cleaned.py" as a function that another Python file can call
+2. Verify count from "crossing_counter_cleaned.py" on several hundred pictures (we should each take a portion of these)
+    * Can anyone easily make a GUI to make this process smoother? I.e. click a "correct" (or "incorrect") button for an image, automatically save that image in a special folder, then automatically load the next image? 
+3. Get map bounding box of "crossing_counter_cleaned.py" to agree with bounding box of API
+
+&nbsp;
+
+# Dataset Processing
+
+[This](https://rock-feller.github.io/intro.html) is a great place to start when considering how our images should be modified. The fundamental philosophy is to create as diverse a dataset as possible while not occupying an inordinate amount of memory. We'll initially consider black-and-white images for space-saving, and then perhaps upgrade to color if that gives better results (color has 3x the data as black-and-white).
+
+Here are things that need doing/exploring:
+
+1. Find an automated way to crop/blur out irrelevant features surrounding the road systems (i.e. greenery and water)
+2. Experiment with SVD decomposition to compress our data set in a smart way while maintaining predictive success
+
+&nbsp;
+
+# Training the Model
+
+If you go to "Filter by Topic" on the Data Science Boot Camp course page on Erdős Institute, you can skip to "12 Week: Neural Networks." There's about 2 hours worth of videos to watch on neural networks. After all of us have watched the videos, we should work on the group problem session together sometime soon.
+
+To experiment, I built a model based on the Erdős Institute model, and I trained it on randomly generated "X" images with a high level of success. I shared some results in our Slack channel. To train a model on 10,000+ images of decent resolution may need some serious computational power. I seem to have access to my university's supercomputer, though I've never used it before.
+
+Here are things that need doing/exploring:
+
+1. Determine how layers for CNN will work (seems to be a lot of guess-and-check)
+2. To remove guess-and-check, visualize images after passing through each filter
+    * Are the correct features being retained between layers?
+3. Learn how to make calls to supercomputer
+
+
+&nbsp;
+
+# Some Reach Goals
+
+Once we've accomplished all of the above, here are some next steps that would be very cool to do:
+
+1. Isotope roads to decrease artificial complexity to get rid of unnecessary crossings
+    * Knowledge of graph theory/knot theory/algebraic topology would be useful
+2. Take direction of traffic (orientation) into account
+3. Indicate <i>where</i> in the image crossings occur by boxing each crossing
+    * These boxes can be programmed to show up in our training data
+4. Explore how low our satellite image resolution can go while maintaining predictive success
+    * This would widely increase our range of application
+5. Is there a correlation between number of crossings and elevation? Or number of crossings and country? Population?
+    * These kinds of statistics can help plan roads according to the area they're in
+6. Can we train our model on satellite images with roads _being hidden_? Namely, can the terrain alone predict the number of road crossings that were built later on?
+
+
---
+++ get_random_coordinates_india.py
@@ -0,0 +1,151 @@
+from crossing_counter import return_crossings
+import numpy as np
+from pyproj import Transformer
+
+#for bounding box: find upper and lower lat/lng bounds based on step; steps are distances in mercator projection
+def get_bounds(lat, lng, step):
+    #make the bounding polygon with vertices in DD
+    flat_transformer = Transformer.from_crs(4326,3857)  #go from DD to mercator projection
+    coord_in_merc = flat_transformer.transform(lat,lng) 
+    lower_lat_bound = coord_in_merc[0]-step
+    upper_lat_bound = coord_in_merc[0]+step
+    lower_lng_bound = coord_in_merc[1]-step
+    upper_lng_bound = coord_in_merc[1]+step
+    #when switching back, need to swap lat/lng
+    curved_transformer = Transformer.from_crs(3857,4326)    #go from mercator projection back to DD
+    SW_point = curved_transformer.transform(lower_lat_bound,lower_lng_bound)
+    NE_point = curved_transformer.transform(upper_lat_bound,upper_lng_bound)
+    return SW_point[0],NE_point[0],SW_point[1],NE_point[1]
+
+
+#initialize list of coordinates to be filled
+list_of_crossings = []
+
+#city step is largest and acts as a large first pass 
+#zoom1 step is smaller and acts as more refined second pass
+#zoom2 step is smallest and acts as most refined final pass (this step corresponds to dataset images)
+city_step = 3000
+zoom1_step = 1500
+zoom2_step = 764.37037
+
+#output stops after accumulating 100 coordinates
+while len(list_of_crossings)<=100:
+    #randomly pick from a rectangular subset of USA
+    lat = np.random.uniform(16.5,48)  #range:(-90,90) usa:(32.5,48)
+    lon = np.random.uniform(73.5,82) #range:(-180,180) usa:(-117,-81.5)
+    print(f"checking for a new city at {lat,lon}")
+    #output of return crossings is either a string, tuple, or integer
+    goal = return_crossings(lat,lon,step = city_step, big_step=city_step)
+
+    #type(goal) is string when large windows makes it unfeasible to calculate edge intersections
+    if type(goal) is str:
+        print("too many edges!")
+        #zoom in from city_step to zoom1_step to reduce number of edges
+        zoom1_crossings = 0
+        counter1 = 0
+        #search within city_step box using smaller, random zoom1_step boxes
+        #search concludes when crossing is located or 10 searches have been performed (there may be no crossing)
+        (lower_lat_bound, upper_lat_bound, lower_lng_bound, upper_lng_bound) = get_bounds(lat, lon, city_step)
+        while zoom1_crossings <= 1 and counter1<=10:            
+            zoom1_lat = np.random.uniform(lower_lat_bound, upper_lat_bound)
+            zoom1_lon = np.random.uniform(lower_lng_bound, upper_lng_bound)
+            zoom1_crossings = return_crossings(zoom1_lat,zoom1_lon,step = zoom1_step, big_step = zoom1_step)
+            counter1 += 1
+
+            if type(zoom1_crossings) is str:
+                print("too many edges after zooming in!!")
+                zoom2_crossings = 0
+                counter2 = 0
+                (lower_lat_bound, upper_lat_bound, lower_lng_bound, upper_lng_bound) = get_bounds(zoom1_lat, zoom1_lon, zoom1_step)
+                while zoom2_crossings <= 1 and counter2<=15:                    
+                    zoom2_lat = np.random.uniform(lower_lat_bound, upper_lat_bound)
+                    zoom2_lon = np.random.uniform(lower_lng_bound, upper_lng_bound)
+                    zoom2_crossings = return_crossings(zoom2_lat,zoom2_lon,step = zoom2_step, big_step = zoom2_step)
+                    counter2 += 1
+                    if type(zoom2_crossings) is str:
+                        print("too many edges after zooming in again!!!") 
+                        break   
+                    if type(zoom2_crossings) is tuple:
+                        zoom2_crossings = len(zoom2_crossings[2])
+                if type(zoom2_crossings) is int:
+                    if zoom2_crossings >= 1:
+                        print("found a zoom2 crossing! appending...")
+                        zoom2_crossings += zoom2_crossings
+                        list_of_crossings.append((zoom2_lat,zoom2_lon))
+                break  
+
+            if type(zoom1_crossings) is tuple:
+                zoom1_crossings = len(zoom1_crossings[2])
+
+            if type(zoom1_crossings) is int:
+                if zoom1_crossings >= 1:
+                    print("found a zoom1 crossing; looking for a zoom2 crossing")
+                    zoom2_crossings = 0
+                    counter2 = 0
+                    (lower_lat_bound, upper_lat_bound, lower_lng_bound, upper_lng_bound) = get_bounds(zoom1_lat, zoom1_lon, zoom1_step)
+                    while zoom2_crossings <= 1 and counter2<=15:                        
+                        zoom2_lat = np.random.uniform(lower_lat_bound, upper_lat_bound)
+                        zoom2_lon = np.random.uniform(lower_lng_bound, upper_lng_bound)
+                        zoom2_crossings = return_crossings(zoom2_lat,zoom2_lon,step = zoom2_step,big_step = 2000)
+                        counter2 += 1
+                        if type(zoom2_crossings) is tuple:
+                            zoom2_crossings = len(zoom2_crossings[2])
+                        #deal with rare (?) edge case of too many edges when zoomed in, but not when zoomed out
+                        if type(zoom2_crossings) is str:
+                            zoom2_crossings=0
+                        if zoom2_crossings >= 1:
+                            print("found a zoom2 crossing! appending...")
+                            zoom2_crossings += zoom2_crossings
+                            list_of_crossings.append((zoom2_lat,zoom2_lon))                    
+                            print(f"so far we have {len(list_of_crossings)} coordinate pairs, and the list is {list_of_crossings}")
+        continue
+
+    #type(goal) is tuple when crossings have been calculated; goal[2] has desired list of crossings
+    if type(goal) is tuple:
+        goal = len(goal[2])
+
+    #type(goal) is int when crossings have been calculated
+    #most coordinates would give goal=0
+    if type(goal) is int:
+        if goal >= 1:
+            print("found a zoom0 crossing; looking for a zoom1 crossing")
+            zoom1_crossings = 0
+            counter1 = 0
+            (lower_lat_bound, upper_lat_bound, lower_lng_bound, upper_lng_bound) = get_bounds(lat, lon, city_step)
+            while zoom1_crossings <= 1 and counter1<=20:                
+                zoom1_lat = np.random.uniform(lower_lat_bound, upper_lat_bound)
+                zoom1_lon = np.random.uniform(lower_lng_bound, upper_lng_bound)
+                zoom1_crossings = return_crossings(zoom1_lat,zoom1_lon,step = zoom1_step,big_step = 2000)
+                if type(zoom1_crossings) is tuple:
+                    zoom1_crossings = len(zoom1_crossings[2]) 
+                counter1 += 1
+                #deal with rare (?) edge case of too many edges when zoomed in, but not when zoomed out
+                if type(zoom1_crossings) is str:
+                    zoom1_crossings=0
+            if zoom1_crossings >= 1:
+                print("found a zoom1 crossing; looking for a zoom2 crossing")
+                zoom2_crossings = 0
+                counter2=0
+                (lower_lat_bound, upper_lat_bound, lower_lng_bound, upper_lng_bound) = get_bounds(zoom1_lat, zoom1_lon, zoom1_step)
+                while zoom2_crossings <= 1 and counter2<=20:
+                    zoom2_lat = np.random.uniform(lower_lat_bound, upper_lat_bound)
+                    zoom2_lon = np.random.uniform(lower_lng_bound, upper_lng_bound)
+                    zoom2_crossings = return_crossings(zoom2_lat,zoom2_lon,step = zoom2_step,big_step = 2000)
+                    if type(zoom2_crossings) is tuple:
+                        zoom2_crossings = len(zoom2_crossings[2]) 
+                    if type(zoom2_crossings) is str:
+                        zoom2_crossings = 0
+                    counter2 += 1 
+                if zoom2_crossings >= 1:
+                    print("found a zoom2 crossing! appending...")
+                    zoom2_crossings += zoom2_crossings
+                    list_of_crossings.append((zoom2_lat,zoom2_lon))
+
+                print(f"so far we have {len(list_of_crossings)} coordinate pairs, and the list is {list_of_crossings}")
+                
+
+#save list of crossings to text file in same directory
+filename = 'output.txt'
+outfile = open(filename, 'w')
+outfile.writelines([str(i)+"," for i in list_of_crossings])
+outfile.close()
